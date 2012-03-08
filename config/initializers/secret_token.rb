5400c8d520411fc3230515a11c581b71745b00a9
itsjoshua <joshuarajan@gmail.com>
2012-03-07 23:54:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueBoxApp::Application.config.secret_token = 'de660867b5b41b9a1c5f5e3b4a344e9462507a6a2aa696a8c22778c786b3cd7f437a813983a97053bead679110b8ef00afe0b6d85ea37267a34d5e3279766f6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BlueBoxApp::Application.config.session_store :cookie_store, :key => '_blueBox_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BlueBoxApp::Application.config.session_store :active_record_store
